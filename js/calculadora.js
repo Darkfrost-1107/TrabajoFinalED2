7909a1eb0deaae97fe455b9b2485026e8e0f0203
Darkfrost-1107 <gabrielbernedo610@gmail.com>
2022-12-19 19:17:18 -0500

    Update v0.3

--- js/calculadora.js
+++ js/calculadora.js
@@ -4,8 +4,7 @@ function cut(i){
 
 function addHistory(op,res){
     document.getElementById('Historial').innerHTML +=
-    '<p class = "operation">' + op + '</p>' +
-    '<p class = "result">' + res + '</p>';
+    '<p class = "operation">' + op +" = " + res + '</p>';
 }
 
 function clearHistory(){
@@ -14,22 +13,16 @@ function clearHistory(){
 
 function Solve(com, print){
     var text = com.replace(/ /g,"");
-    alert(text);
     var text = com.replace(/%/g,"/100");
 
     let op, op1, op2, res, pattern = /(\(([0-9\.\+\-\*\/]+)?\))/;
     while(pattern.test(text)){
         op = pattern.exec(text);
-            alert(op);
             text = text.replace(op[1],Solve(op[2]));
-            alert("finito");
-            alert(text);
-            alert("continue");
         
     }
     for(pattern = /(^|\+|\-|\/|\*)(-?[0-9\.]+)(\/)(-?[0-9\.]+)($|\+|\-|\/|\*)/; pattern.test(text);){
         op = pattern.exec(text);
-        alert(op);
         op1 = op[2];
         op2 = op[4];
         res = Operate(op1,op[3],op2);
@@ -37,7 +30,6 @@ function Solve(com, print){
     }
     for(pattern = /(^|\+|\-|\*)(-?[0-9\.]+)(\*)(-?[0-9\.]+)($|\+|\-|\*)/; pattern.test(text);){
         op = pattern.exec(text);
-        alert(op);
         op1 = op[2];
         op2 = op[4];
         res = Operate(op1,op[3],op2);
@@ -45,7 +37,6 @@ function Solve(com, print){
     }
     for(pattern = /(^|\+|\-)(-?[0-9\.]+)(\-)(-?[0-9\.]+)($|\+|\-)/; pattern.test(text);){
         op = pattern.exec(text);
-        alert(op);
         op1 = op[2];
         op2 = op[4];
         res = Operate(op1,op[3],op2);
@@ -53,15 +44,12 @@ function Solve(com, print){
     }
     for(pattern = /(^|\+)(-?[0-9\.]+)(\+)(-?[0-9\.]+)($|\+)/; pattern.test(text);){
         op = pattern.exec(text);
-        alert(op);
         op1 = op[2];
         op2 = op[4];
         res = Operate(op1,op[3],op2);
         text = text.replace(op1 + op[3] + op2,res);
     }
     text = ChompPlus(text);
-    
-    alert(text + ": fin");
     let pat = /^-?[0-9\. ]+$/g;
     if(pat.test(text)){
         if(print){ addHistory(com,text); }
